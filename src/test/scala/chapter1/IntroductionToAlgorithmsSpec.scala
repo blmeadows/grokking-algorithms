209bcbbe2f4712e86a72320f935b01e35f075fa3
Britany Meadows <letbritcode@gmail.com>
2021-06-20 19:57:18 -0500

    chapter 1 test update

--- src/test/scala/chapter1/IntroductionToAlgorithmsSpec.scala
+++ src/test/scala/chapter1/IntroductionToAlgorithmsSpec.scala
@@ -5,16 +5,21 @@ import org.scalacheck.Prop.forAll
 
 class IntroductionToAlgorithmsSpec extends munit.ScalaCheckSuite {
   test("binary search with Int") {
-    val list = Array(1, 3, 5, 7, 9)
-    assert(binarySearch(list, 3).contains(1))
-    assert(binarySearch(list, -1).isEmpty)
+    val array = Array(1, 3, 5, 7, 9)
+    assert(binarySearch(array, 3).contains(1))
+    assert(binarySearch(array, -1).isEmpty)
   }
 
   test("binary search with Float") {
-    val list =
+    val array =
       Array(BigDecimal(1.5), BigDecimal(3.5), BigDecimal(5.5), BigDecimal(7.5), BigDecimal(9.5))
-    assert(binarySearch(list, BigDecimal(3.5)).contains(1))
-    assert(binarySearch(list, BigDecimal(-1.0)).isEmpty)
+    assert(binarySearch(array, BigDecimal(3.5)).contains(1))
+    assert(binarySearch(array, BigDecimal(-1.0)).isEmpty)
+  }
+
+  test("binary search compared to indexOf") {
+    val array = Array(1, 3, 5, 7, 9)
+    assert(binarySearch(array, 3).get == array.indexOf(3))
   }
 
   property("binary search pbt Int") {
@@ -24,19 +29,8 @@ class IntroductionToAlgorithmsSpec extends munit.ScalaCheckSuite {
         val index = 0
         val itemToFind = processedArray(index)
         assert(binarySearch(processedArray, itemToFind).contains(index))
+        assert(binarySearch(processedArray, itemToFind).get == processedArray.indexOf(itemToFind))
         assert(binarySearch(processedArray, -1).isEmpty)
-      }
-    }
-  }
-
-  property("binary search pbt BigDecimal") {
-    forAll { (array: Array[BigDecimal]) =>
-      val processedArray = array.distinct.sorted.filterNot(_ == -1.5)
-      if (processedArray.nonEmpty) {
-        val index = 0
-        val itemToFind = processedArray(index)
-        assert(binarySearch(processedArray, itemToFind).contains(index))
-        assert(binarySearch(processedArray, BigDecimal(-1.5)).isEmpty)
       }
     }
   }
